351678fa99ba88da0a9ef4af40f276e2ecae9f9f
Omni <aidanali65@gmail.com>
2025-10-08 20:48:37 -0400

    update to homepage

--- src/components/GlowingDemo/GlowingDemo.tsx
+++ src/components/GlowingDemo/GlowingDemo.tsx
@@ -9,28 +9,28 @@ export function GlowingEffectDemoSecond() {
             <GridItem
                 area="md:[grid-area:1/1/2/7] xl:[grid-area:1/1/2/5]"
                 icon={<Box className="h-4 w-4 text-black dark:text-neutral-400" />}
-                title="Do things the right way"
-                description="Running out of copy so I'll write anything."
+                title="PC Builder Guides"
+                description="Guides an tips to help build your Dream Gaming Setup."
             />
 
             <GridItem
                 area="md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]"
                 icon={<Settings className="h-4 w-4 text-black dark:text-neutral-400" />}
-                title="The best AI code editor ever."
+                title="Windows Optimization"
                 description="Yes, it's true. I'm not even kidding. Ask my mom if you don't believe me."
             />
 
             <GridItem
                 area="md:[grid-area:2/1/3/7] xl:[grid-area:1/5/3/8]"
                 icon={<Lock className="h-4 w-4 text-black dark:text-neutral-400" />}
-                title="You should buy Aceternity UI Pro"
+                title="Graphics Settings & Optimization"
                 description="It's the best money you'll ever spend"
             />
 
             <GridItem
                 area="md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]"
                 icon={<Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />}
-                title="This card is also built by Cursor"
+                title=""
                 description="I'm not even kidding. Ask my mom if you don't believe me."
             />
 
