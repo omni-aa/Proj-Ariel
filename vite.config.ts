69bfddb73eaf1c230a07ea8068a5b159e11e646a
Omni <aidanali65@gmail.com>
2025-10-26 01:23:58 -0400

    Adding a simple icon

--- vite.config.ts
+++ vite.config.ts
@@ -19,17 +19,17 @@ export default defineConfig({
                 theme_color: '#ffffff',
                 icons: [
                     {
-                        src: 'pwa-192x192.png',
+                        src: 'generative.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: 'pwa-512x512.png',
+                        src: 'generative.png',
                         sizes: '512x512',
                         type: 'image/png',
                     },
                     {
-                        src: 'pwa-512x512.png',
+                        src: 'generative.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'any maskable',
